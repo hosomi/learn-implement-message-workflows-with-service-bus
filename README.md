a56bf2b6922163fd3c3c5a0b52d96c0bbeef0a18
hosomi <kiyotake.hosomi@outlook.jp>
2020-08-04 13:01:16 +0900

    learn from VisualStudio2019

--- README.md
+++ README.md
@@ -1,1 +1,56 @@
 # learn-implement-message-workflows-with-service-bus
+
+https://docs.microsoft.com/ja-jp/learn/modules/implement-message-workflows-with-service-bus/
+
+## 特記事項
+
+VisualStudio 2019 で動作確認をするためのプロジェクトです。  
+（接続文字列以外は実装済みです。）  
+Microsoft.Azure.ServiceBus の Nuget は 3.1.0 から 4.1.3 に上げています、動作確認済みです。  
+
+
+## Azure CLI 
+
+[Azure CLI のインストール Microsoft Docs](https://docs.microsoft.com/ja-jp/cli/azure/install-azure-cli?view=azure-cli-latest)
+
+
+Azure にログインする：
+
+```powershell
+az login
+```
+
+
+作成したサービスバスの接続文字列を取得する：  
+
+```powershell
+PS learn-implement-message-workflows-with-service-bus> az servicebus namespace authorization-rule keys list `
+    --resource-group <your-resource-group>`
+    --name RootManageSharedAccessKey `
+    --query primaryConnectionString `
+    --output tsv `
+    --namespace-name <your-servicebus-namespace-name>
+
+```
+
+サービスバスのキューのメッセージ数を取得する：  
+
+```powershell
+PS learn-implement-message-workflows-with-service-bus> az servicebus queue show `
+    --resource-group <your-resource-group> `
+    --name salesmessages `
+    --query messageCount `
+    --namespace-name <your-servicebus-namespace-name> `
+```
+
+サービスバスのトピックとサブスクリプションのメッセージ数を取得する：  
+
+```powershell
+PS learn-implement-message-workflows-with-service-bus> az servicebus topic subscription show `
+    --resource-group <your-resource-group>  `
+    --namespace-name <your-servicebus-namespace-name> `
+    --topic-name salesperformancemessages `
+    --name EuropeAndAfrica  `
+    --query messageCount
+```
+
---
+++ performancemessagesender/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Azure.ServiceBus;
+
+namespace performancemessagesender
+{
+    class Program
+    {
+        const string ServiceBusConnectionString = "";
+        const string TopicName = "salesperformancemessages";
+        static ITopicClient topicClient;
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Sending a message to the Sales Performance topic...");
+
+            SendPerformanceMessageAsync().GetAwaiter().GetResult();
+
+            Console.WriteLine("Message was sent successfully.");
+
+        }
+
+        static async Task SendPerformanceMessageAsync()
+        {
+            // Create a Topic Client here
+            topicClient = new TopicClient(ServiceBusConnectionString, TopicName);
+
+            // Send messages.
+            try
+            {
+                // Create and send a message here
+                string messageBody = $"Total sales for Brazil in August: $13m.";
+                Console.WriteLine($"Sending message: {messageBody}");
+                var message = new Message(Encoding.UTF8.GetBytes(messageBody));
+                await topicClient.SendAsync(message);
+                await topicClient.CloseAsync();
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine($"{DateTime.Now} :: Exception: {exception.Message}");
+            }
+
+            // Close the connection to the topic here
+        }
+    }
+}
